0f54a99e6d14b9f283421219406546a24d81f913
ali-turabi <159777576+ali-turabi@users.noreply.github.com>
2025-05-19 02:06:01 +0300

    for abed

--- hopeconnect-backend/app.js
+++ hopeconnect-backend/app.js
@@ -9,14 +9,6 @@ const bodyParser = require('body-parser');
 const orphanUpdateRoutes = require('./routes/orphanUpdateRoutes');
 const sponsorshipRoutes = require('./routes/sponsorshipRoutes');
 const donationRoutes = require('./routes/donationRoutes');
-const reportRoutes = require('./routes/reportRoutes');
-const volunteerRoutes = require('./routes/volunteerRoutes');
-const requestRoutes = require('./routes/requestRoutes')
-const assignmentRoutes = require('./routes/assignmentRoutes');
-const expenditureRoutes = require('./routes/expenditureRoutes');
-const impactReportRoutes = require('./routes/impactReportRoutes');
-const verificationRoutes = require('./routes/verificationRoutes');
-const reviewRoutes = require('./routes/reviewRoutes');
 const reviewPlatformRoutes = require('./routes/reviewPlatformRoutes'); // Updated filename
 const budgetRoutes = require('./routes/budgetRoutes');
 
@@ -33,14 +25,6 @@ app.use('/api/reports', reportRoutes);
 app.use('/api', reviewPlatformRoutes);
 app.use('/api', budgetRoutes);
 
-//mmmmmmmmmmmmmmmmmmmmmmmmmmm
-app.use('/api/volunteers', volunteerRoutes);
-app.use('/api/requests', requestRoutes);
-app.use('/api/assignments', assignmentRoutes);
-app.use('/api/expenditures', expenditureRoutes);
-app.use('/api/reports', impactReportRoutes);
-app.use('/api/verifications', verificationRoutes);
-app.use('/api/reviews', reviewRoutes);
 
 
 const PORT = process.env.PORT || 3000;
