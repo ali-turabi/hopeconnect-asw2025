329e9cdeb8a8be4fe2cd64ab1f62b7813eda3631
ali-turabi <159777576+ali-turabi@users.noreply.github.com>
2025-05-19 00:34:17 +0300

    mmmm

--- hopeconnect-backend/routes/reviewPlatformRoutes.js
+++ hopeconnect-backend/routes/reviewPlatformRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const reviewPlatformController = require('../controllers/reviewPlatformController'); // Updated filename
+const { authenticate, authorizeAdmin } = require('../middleware/auth');
 
-router.post('/reviews', reviewPlatformController.createReview);
-router.get('/reviews', reviewPlatformController.getAllReviews);
+router.post('/reviewsPlatform', reviewPlatformController.createReview);
+router.get('/reviewsPlatform',authenticate, authorizeAdmin, reviewPlatformController.getAllReviews);
 
 module.exports = router;
